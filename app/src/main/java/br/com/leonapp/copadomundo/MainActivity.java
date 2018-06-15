951cb90f12467e22e883454f81d9a9a98fa6feaa
leosouzajr <33257894+leosouzajr@users.noreply.github.com>
2018-06-15 11:39:11 -0300

    Initial commit

---
+++ app/src/main/java/br/com/leonapp/copadomundo/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.leonapp.copadomundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
